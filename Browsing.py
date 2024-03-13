72a36d0d323822028b8b611a4a7c8772496db801
k31jinny <32210207a@dankook.ac.kr>
2024-03-13 13:13:03 -0400

    Browsing

--- Browsing.py
+++ Browsing.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
@@ -7,7 +8,70 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 url = 'https://google.com'
-driver.get(url)
+driver.get(url) 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options =Options()   
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+url = 'https://google.com'
+driver.get(url) 
+
+
+'''
+<textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" 
+jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="true" aria-haspopup="false" 
+autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" 
+spellcheck="false" data-ved="0ahUKEwj3-uuU9O-EAxUsGDQIHR9QBscQ39UDCAY" aria-activedescendant="" style=""></textarea>
+'''
+
+
+#driver.find_element(By.CLASS_NAME,"gLFyf").send_keys("블랙핑크")
+#time.sleep(1)
+#driver.find_element(By.ID, "APjFqb").send_keys("뉴진스")
+#time.sleep(1)
+#driver.find_element(By.NAME, "q").send_keys("트와이스")
+
+'''//*[@id="APjFqb"]'''
+#driver.find_element(By.XPATH,'//*[@id="APjFqb"]').send_keys("security intern")
+#time.sleep(1)
+driver.find_element(By.CSS_SELECTOR,"[title ='Search']").send_keys("security intern")
+time.sleep(1)
+
+
+#driver.find_element(By.LINK_TEXT, "Sign in").click()
+#driver.find_element(By.PARTIAL_LINK_TEXT, "Sign").click()
+
+
+driver.find_element(By.CSS_SELECTOR,"[title ='Search']").send_keys(Keys.ENTER)
+time.sleep(1)
+
+'''<span class="gsmt ernJ4c" aria-level="2" role="heading" 
+aria-label="Google interactive job search.">Jobs</span>'''
+
+driver.find_element(By.LINK_TEXT,"Jobs").click()
+time.sleep(1)
 
 
 
+navs = driver.find_elements(By.CSS_SELECTOR, ".BjJfJf")
+for nav in navs:
+    print(nav.get_attribute("outerHTML"))
+    print()
+    
+
+
+
+'''
+<textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="true" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwj3-uuU9O-EAxUsGDQIHR9QBscQ39UDCAY" aria-activedescendant="" style=""></textarea>
+'''
+
+
+driver.find_element(By.CLASS_NAME,"gLFyf").send_keys("블랙핑크")
+time.sleep(1)
+driver.find_element(By.ID, "APjFqb").send_keys("뉴진스")
