f5125185bb225593e4f6c19d7457bf506c5ea50d
Sojin Kwak(곽소진) <130416168+k31jinny@users.noreply.github.com>
2024-03-12 23:34:44 -0400

    code changes

--- Browsing.py
+++ Browsing.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
@@ -7,7 +8,14 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 url = 'https://google.com'
-driver.get(url)
+driver.get(url) 
 
 
+'''
+<textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="true" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwj3-uuU9O-EAxUsGDQIHR9QBscQ39UDCAY" aria-activedescendant="" style=""></textarea>
+'''
 
+
+driver.find_element(By.CLASS_NAME,"gLFyf").send_keys("블랙핑크")
+time.sleep(1)
+driver.find_element(By.ID, "APjFqb").send_keys("뉴진스")
